576cad35a1032b4c59df69c9c301275f99ccc01d
SergeyGSA <goreslavskiy.sergey@gmail.com>
2022-08-18 19:06:11 +0300

    added contacts list

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {ChatComponent} from 'src/app/chat/components/chat/chat.component'
-import {SearchComponent} from 'src/app/chat/components/search/search.component'
+import {SearchComponent} from 'src/app/chat/components/search/search.component';
+import { ContactComponent } from './components/contact/contact.component'
 
 @NgModule({
-  declarations: [ChatComponent, SearchComponent],
+  declarations: [ChatComponent, SearchComponent, ContactComponent],
   imports: [CommonModule],
 })
 export class ChatModule {}
