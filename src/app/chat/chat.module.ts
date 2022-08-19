c5e12fe7277ce5be4978fb916ad8b659f35704fd
SergeyGSA <goreslavskiy.sergey@gmail.com>
2022-08-19 13:16:48 +0300

    added message-input component

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,12 +1,19 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {ChatComponent} from 'src/app/chat/components/chat/chat.component'
-import {SearchComponent} from 'src/app/chat/components/search/search.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { MessageComponent } from './components/message/message.component'
+import {SearchComponent} from 'src/app/chat/components/search/search.component'
+import {ContactComponent} from './components/contact/contact.component'
+import {MessageComponent} from './components/message/message.component'
+import {MessageInputComponent} from './components/message-input/message-input.component'
 
 @NgModule({
-  declarations: [ChatComponent, SearchComponent, ContactComponent, MessageComponent],
+  declarations: [
+    ChatComponent,
+    SearchComponent,
+    ContactComponent,
+    MessageComponent,
+    MessageInputComponent,
+  ],
   imports: [CommonModule],
 })
 export class ChatModule {}
