889277cfbe6b0eb430d2a9e2310041588834ca13
SergeyGSA <goreslavskiy.sergey@gmail.com>
2022-08-19 10:50:48 +0300

    added message component

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {ChatComponent} from 'src/app/chat/components/chat/chat.component'
 import {SearchComponent} from 'src/app/chat/components/search/search.component';
-import { ContactComponent } from './components/contact/contact.component'
+import { ContactComponent } from './components/contact/contact.component';
+import { MessageComponent } from './components/message/message.component'
 
 @NgModule({
-  declarations: [ChatComponent, SearchComponent, ContactComponent],
+  declarations: [ChatComponent, SearchComponent, ContactComponent, MessageComponent],
   imports: [CommonModule],
 })
 export class ChatModule {}
