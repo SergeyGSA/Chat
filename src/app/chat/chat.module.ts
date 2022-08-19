9cb34f92d728296fabb1fc0d54e7a17a1738663f
SergeyGSA <goreslavskiy.sergey@gmail.com>
2022-08-19 13:39:01 +0300

    added angular-in-memory

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,5 +1,9 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
+import {HttpClientModule} from '@angular/common/http'
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api'
+import {InMemoryDataService} from './services/in-memory-data.service'
+
 import {ChatComponent} from 'src/app/chat/components/chat/chat.component'
 import {SearchComponent} from 'src/app/chat/components/search/search.component'
 import {ContactComponent} from './components/contact/contact.component'
@@ -14,6 +18,12 @@ import {MessageInputComponent} from './components/message-input/message-input.co
     MessageComponent,
     MessageInputComponent,
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
 })
 export class ChatModule {}
