2cbdfe6e85846fb0e55d81ca1938bb97a3bae7f5
SergeyGSA <goreslavskiy.sergey@gmail.com>
2022-08-26 13:16:53 +0300

    $

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./src/DB/db.json')
-const middlewares = jsonServer.defaults({static: './build'})
-const PORT = 3000
+const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 3000
+
 server.use(middlewares)
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  })
-)
 server.use(router)
+
 server.listen(PORT, () => console.log('Server has been started...'))
+
+// const data = require('./db.json')
+
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router(data)
+// const middlewares = jsonServer.defaults()
+// const PORT = 3000
+
+// server.use(middlewares)
+// server.use(router)
+// server.listen(PORT, () => console.log('Server has been started...'))
